ebc527f8957d466561b4283745bd6ba6ec52577e
Abdelrahman <abdelrahman@knawat.com>
2020-01-02 17:47:26 +0300

    feat: add taxs api

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import orders from './api/orders';
 import products from './api/products';
 import shipping from './api/shipping';
 import stores from './api/stores';
+import taxes from './api/taxes';
 
 KnawatMP._addToPrototype(categories);
 KnawatMP._addToPrototype(invoices);
@@ -16,5 +17,6 @@ KnawatMP._addToPrototype(orders);
 KnawatMP._addToPrototype(products);
 KnawatMP._addToPrototype(shipping);
 KnawatMP._addToPrototype(stores);
+KnawatMP._addToPrototype(taxes);
 
 module.exports = KnawatMP;
